2ebedd7ac5d7c9bdb3870dccf5d5b4c10de261d3
Vicente Deluca <deluca.vicente@gmail.com>
2024-05-07 23:21:44 -0300

    first commit

---
+++ src/app/map/map.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit, OnDestroy } from '@angular/core';
+import { Map } from 'maplibre-gl';
+
+@Component({
+  selector: 'app-map',
+  standalone: true,
+  imports: [],
+  templateUrl: './map.component.html',
+  styleUrl: './map.component.css'
+})
+export class MapComponent implements OnInit, AfterViewInit, OnDestroy {
+  map: Map | undefined;
+
+  @ViewChild('map')
+  private mapContainer!: ElementRef<HTMLElement>;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  ngAfterViewInit() {
+    const initialState = { lng: 139.753, lat: 35.6844, zoom: 14 };
+
+    this.map = new Map({
+      container: this.mapContainer.nativeElement,
+      style: `https://api.maptiler.com/maps/streets-v2/style.json?key=l9gANY9FtQqwITBcgpHn`,
+      center: [initialState.lng, initialState.lat],
+      zoom: initialState.zoom
+    });
+
+  }
+
+  ngOnDestroy() {
+    this.map?.remove();
+  }
+
+}
